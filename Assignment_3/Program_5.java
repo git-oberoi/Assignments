2652435c3caca7746fc6f1750f6d5790b5a4beda
git-oberoi <shivamraj91150@gmail.com>
2023-12-02 00:39:06 +0530

    Program_5 Completed

--- Assignment_3/Program_5.java
+++ Assignment_3/Program_5.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Program_5 {
     public static void main(String[] args) {
-        
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Input the year: ");
+
+   int year = sc.nextInt();
+
+   boolean LeapYr = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+   System.out.println(year + " is a leap year: " + LeapYr);
+
+   sc.close();
+
     }
 }
