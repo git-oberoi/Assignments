80d2a14ecc8f00028f87813816a5efc9d5645513
git-oberoi <shivamraj91150@gmail.com>
2023-12-02 17:04:57 +0530

    Program_6 Completed

---
+++ Assignment_3/Program_6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program_6 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+System.out.println(" Enter your monthly unit: ");
+
+    double unit = sc.nextDouble();
+
+    if (unit <= 50) {
+        System.out.println(" Your bill is: " + unit * 3);
+    }
+    else if (unit > 50 || unit <= 200) {
+        System.out.println(" Your bill is: " + unit * 4.80);
+    }
+    else if (unit > 200 || unit <= 400) {
+        System.out.println(" Your bill is: " + unit * 5.80);
+    }
+    else if (unit > 400) {
+        System.out.println(" Your bill is: " + unit * 6.20);
+    }
+    else {
+        System.out.println("Enter correct unit");
+    }
+    sc.close();
+}    
+}
