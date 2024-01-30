99b7d32de989773aa5614de714a2399fe007cd2e
Shivam Raj <39915803+git-oberoi@users.noreply.github.com>
2024-01-30 19:32:35 +0530

    Update program_7.java

--- Assignment_2/program_7.java
+++ Assignment_2/program_7.java
@@ -1,19 +1,24 @@
-public class program_7 {
+public class PowerTable {
     public static void main(String[] args) {
-        float a, b;
-System.out.println("a  b pow(a, b)");
-            a = 1;
-            b = 2;
-System.out.println((int)a + "  " + (int)b + "  " + (int)Math.pow(a, b)); a++;
-b++;
-System.out.println((int)a + "  " + (int)b + "  " + (int)Math.pow(a, b)); a++;
-b++;
-System.out.println((int)a + "  " + (int)b + "  " + (int)Math.pow(a, b)); a++;
-b++;
-System.out.println((int)a + "  " + (int)b + "  " + (int)Math.pow(a, b)); a++;
-b++;
-System.out.println((int)a + "  " + (int)b + "  " + (int)Math.pow(a, b));
+        System.out.println("a\tb\tpow(a, b)");
+        System.out.println("-------------------");
 
+        int a = 1;
+        int b = 2;
+        int result1 = (int) Math.pow(a++, b++);
+        System.out.println(a + "\t" + b + "\t" + result1);
+
+        int result2 = (int) Math.pow(a++, b++);
+        System.out.println(a + "\t" + b + "\t" + result2);
+
+        int result3 = (int) Math.pow(a++, b++);
+        System.out.println(a + "\t" + b + "\t" + result3);
+
+        int result4 = (int) Math.pow(a++, b++);
+        System.out.println(a + "\t" + b + "\t" + result4);
+
+        int result5 = (int) Math.pow(a++, b++);
+        System.out.println(a + "\t" + b + "\t" + result5);
     }
-    
 }
+
